fb730caf112e99b455c6dfeabb2f0b949fc4d060
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2016-10-08 11:47:58 -0400

    Create default.php
    Hello! This is my first pull to this repo!
    A couple things
    1. I changed some of the format of the code, don't worry though:
    2. This should be the only page on the site that will use fullpage.js (though other pages will use materialize and scrollreveal)
    3. I would also like to request to change the css llibrary from bootstrap to materialize. I would also like to use CDN for everything (it might be faster)

---
+++ default.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" href="/img/favicon.png" type="image/png" />
+  <title>Oakton Computer Science Club</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/jquery.fullPage.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/vendors/jquery.easings.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/vendors/scrolloverflow.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/jquery.fullPage.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <style>
+  </style>
+</head>
+<body>
+  <div id="fullpage">
+    <div class="section" style="background-color:#2196F3;">
+      <center><h1 class="title">OaktonCompSci</h1></center><br/>
+      <center> <i class="title material-icons">polymer</i></center><br/>
+      <center><small class="words">Oakton's very own computer science club!</small>
+      </center><div class="down1" style="position: fixed; top:80%; width:100%;"><center>Scroll Down</center></div>
+    </div>
+    <div class="section" style="background-color:#42A5F5">
+      <center><h1>Welcome to our website!</h1></center>
+      <center>Here, you can view some recent updates, or simply browse some pages to see what we are all about!</center>
+    </div>
+    <div class="section" style="background-color:#64B5F6">
+          <nav style="height:8%;"><div class="blue nav-wrapper"><a style="position:relative; left:40%;" href="#achievements" class="brand-logo">Achievements</a></div></nav>
+<br/><br/>
+<div class="container">
+<div class="row">
+  <div class="col s3 m4">
+      <div class="card z-depth-3">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" alt="A picture of something relating to what we achieved" src="plz_put_a_pic_of_achievement.jpg">
+        </div>
+        <div class="card-content activator">
+          <span class="card-title activator grey-text text-darken-4">Short Competition Name (like ACSL)</span>
+          <p>Short Description. <i class="material-icons">open_in_new</i></p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Long Competition Name (American Computer Science League)<i class="material-icons right">close</i></span>
+          <p>Longer desc.!</p>
+        </div>
+      </div>
+    </div>
+
+  <div class="col s3 m4">
+      <div class="card z-depth-3">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" alt="A picture of something relating to what we achieved" src="plz_put_a_pic_of_achievement.jpg">
+        </div>
+        <div class="card-content activator">
+          <span class="card-title activator grey-text text-darken-4">Short Competition Name (like ACSL)</span>
+          <p>Short Description. <i class="material-icons">open_in_new</i></p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Long Competition Name (American Computer Science League)<i class="material-icons right">close</i></span>
+          <p>Longer desc.!</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3 m4">
+        <div class="card z-depth-3">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" alt="A picture of something relating to what we achieved" src="plz_put_a_pic_of_achievement.jpg">
+          </div>
+          <div class="card-content activator">
+            <span class="card-title activator grey-text text-darken-4">Short Competition Name (like ACSL)</span>
+            <p>Short Description. <i class="material-icons">open_in_new</i></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Long Competition Name (American Computer Science League)<i class="material-icons right">close</i></span>
+            <p>Longer desc.!</p>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+
+    </div>
+    <div class="section" style="background-color:#90CAF9;">
+      <div class="card horizontal">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="ira.jpg">
+        </div>
+        <div class="card-content activator">
+          <span class="card-title grey-text text-darken-4">Work History</span>
+          <p>I started and ran my own lemonade stand during various time in various summer vacations. Click for more.</p>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Work History<i class="right">X</i></span>
+          <p>I also sell art, fish, and origami using the PrizePoints currency to my brother, parents, and friends</p>
+        </div>
+      </div>
+    </div>
+    <div class="section" style="background-color:#BBDEFB;"><h1><center>Contact her brother dgramopadhye@gmail.com</center></h1></div>
+  </div>
+
+  <div class="fixed-action-btn" style="bottom: 40px; right: 24px;">
+  <a class="btn-floating btn-large red">
+    <i class="large material-icons">launch</i>
+  </a>
+  <ul>
+    <li><a href="login.php" class="btn-floating red"><i class="material-icons">dashboard</i></a></li>
+    <li><a href="announcements.php" class="btn-floating yellow darken-1"><i class="material-icons">message</i></a></li>
+    <li><a href="login.php" class="btn-floating green"><i class="material-icons">group_work</i></a></li>
+    <li><a href="http://github.com/" class="btn-floating blue"><i class="material-icons">polymer</i></a></li>
+  </ul>
+</div>
+
+
+  <script src="https://cdn.jsdelivr.net/scrollreveal.js/3.3.1/scrollreveal.min.js"></script>
+  <script>
+  window.sr=ScrollReveal();
+  sr.reveal('.title', 400);
+  sr.reveal('.words', { delay: 500 });
+  sr.reveal('.down1', { delay: 700 });
+  sr.reveal('.achievements', { delay: 400 });
+
+  $('#fullpage').fullpage({
+    anchors: ['home', 'welcome', 'achievements', 'links', 'services', 'contact'],
+    menu: '#bar',
+    afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+            //screw case statements
+            if(index == 1){
+              document.getElementsByTagName("title")[0].innerHTML="Oakton Computer Science Club"
+            }
+            if(index == 2){
+              setTimeout(function(){$.fn.fullpage.moveSectionDown();}, 4000);
+            }
+            if(index == 3){
+              document.getElementsByTagName("title")[0].innerHTML="OCS - Achievements"
+            }
+
+            if(index == 4){
+              document.getElementsByTagName("title")[0].innerHTML="OCS - Links"
+            }
+
+            if(index == 5){
+              document.getElementsByTagName("title")[0].innerHTML="OCS - Services" //more projects for non-profits
+            }
+
+            if(index == 6){
+              document.getElementsByTagName("title")[0].innerHTML="OCS - Contact"
+            }
+
+            //Template made by dGRMAOP Based on dGRAMOP's home page
+          }
+});
+</script>
+</body>
+</html>
