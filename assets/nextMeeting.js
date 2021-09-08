b59ddb3588fcfa1fd9c1d6fb0125cf5e35b639bd
ethan-s-personal <84857534+ethan-s-personal@users.noreply.github.com>
2021-09-08 16:02:32 -0400

    Update nextMeeting.js

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
-export var time = "September 8th 3-4pm";
-export var loc = "Lunch Room";
-export var description = "Come to the interest meeting for the Oatkon CSCS club! Everyone is invited, even if you don't have any prior experience.";
+export var time = "September 15th 3-4pm";
+export var loc = "TBD";
+export var description = "Check our discord for the lastest updates on what is planned.";
