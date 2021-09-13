3ed22c929e96ac40c416776dbfe67bb8f6958d49
ethan-s-personal <84857534+ethan-s-personal@users.noreply.github.com>
2021-09-13 17:27:22 -0400

    Update nextMeeting.js

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
 export var time = "September 15th 3-4pm";
-export var loc = "TBD";
+export var loc = "Ms. Kumpf’s Room 1215";
 export var description = "Check our discord for the lastest updates on what is planned.";
