7fb222e9778905631dd682788e88f06d312e6c7c
ethan-s-personal <84857534+ethan-s-personal@users.noreply.github.com>
2021-09-29 18:07:18 -0400

    Update nextMeeting.js

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
-export var time = "September 29nd 3-4pm";
+export var time = "October 6th 3-4pm";
 export var loc = "Mr. Larson's Room 1707";
-export var description = "RadfordCTF stuff I think; check Discord.";
+export var description = "Club Projects!!! (and more cyberpatriot details probably)";
