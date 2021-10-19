e88a610154cc5d2ffd5d909e92f70dc9f5092e26
ethan-s-personal <84857534+ethan-s-personal@users.noreply.github.com>
2021-10-19 15:35:11 -0400

    Update nextMeeting.js

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
 export var time = "October 20th 3-4pm";
-export var loc = "TBD";
+export var loc = "1210 Mr Collins Room";
 export var description = "Presentation on Git/Github Deskop and some project work time. Note we will not be meeting on the 13th.";
