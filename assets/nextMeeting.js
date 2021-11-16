1be7f0b748f117bd1edab3590012eca084bdd7bb
Ethan <noreply@example.com>
2021-11-16 11:01:49 -0500

    example commit

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
-export var time = "November 10 3-4pm";
-export var loc = "Mr. Larsons Room 1707";
-export var description = "AI generation related discussion";
+export var time = "TBD";
+export var loc = "TBD";
+export var description = "TBD";
