462e81597d10d5bcdee23d5247020ccf098305d2
GitServant <noreply@example.com>
2021-11-19 22:14:09 +0000

    Updating next meeting.

--- assets/nextMeeting.js
+++ assets/nextMeeting.js
@@ -1,3 +1,3 @@
-export var time = "&365216886771941396> epic bot announcement!!! Use the bot to update the website. Format is `?Update";
-export var loc = "Time";
-export var description = "Location";
+export var time = "No Meeting This Week";
+export var loc = "Eating Turkey";
+export var description = "Have a nice break!";
